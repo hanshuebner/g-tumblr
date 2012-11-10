95028c77b3b48e575ad07e7fc6a4b1f5bb690da5
Hans Huebner <hans.huebner@gmail.com>
2012-11-10 08:17:26 -0500

    implement tumblr posting, does not work yet

--- g-tumblr.lisp
+++ g-tumblr.lisp
@@ -6,7 +6,8 @@
 
 (in-package :g-tumblr)
 
-(defparameter +max-width+ 1200)
+(defparameter *max-width* 1200)
+(defparameter *config-pathname* "~/.g-tumblr-config.lisp")
 
 (defun get-image-orientation (pathname)
   (zpb-exif:parsed-exif-value :Orientation (zpb-exif:make-exif pathname)))
@@ -19,8 +20,8 @@
                                            (:rotated-90 90)
                                            (:rotated-270 270)
                                            (:normal nil)))
-        (let* ((resizep (< +max-width+ original-height))
-               (width (min +max-width+ original-height))
+        (let* ((resizep (< *max-width* original-height))
+               (width (min *max-width* original-height))
                (height (floor (* original-width (/ width original-height)))))
           (cl-gd:with-image (rotated-image original-height original-width t)
             (cl-gd:copy-image original-image rotated-image 0 0 (floor original-height 2) (floor original-width 2) original-width original-height
@@ -29,8 +30,8 @@
               (cl-gd:copy-image rotated-image rescaled-image 0 0 0 0 original-height original-width
                                 :dest-width width :dest-height height :resample resizep :resize resizep)
               (cl-gd:write-image-to-file output-pathname :image rescaled-image :if-exists :supersede))))
-        (let* ((resizep (< +max-width+ original-width))
-               (width (min +max-width+ original-width))
+        (let* ((resizep (< *max-width* original-width))
+               (width (min *max-width* original-width))
                (height (* original-height (/ width original-width))))
           (cl-gd:with-image (rescaled-image width height t)
             (cl-gd:copy-image original-image rescaled-image 0 0 0 0 original-width original-height
@@ -75,7 +76,7 @@
 ;; standard OAuth flow, which is implemented by the below or by
 ;; https://gist.github.com/2603387
 
-(defun configure (consumer-key consumer-secret)
+(defun configure (blog-name consumer-key consumer-secret)
   (let* ((consumer-token (cl-oauth:make-consumer-token :key consumer-key :secret consumer-secret))
          (callback-uri (format nil "http://localhost:~A/" *hunchentoot-port*))
          (request-token (cl-oauth:obtain-request-token *get-request-token-endpoint* consumer-token
@@ -94,17 +95,51 @@
             (cl-oauth:authorize-request-token request-token)
             (setf (cl-oauth:request-token-verification-code request-token) oauth-verifier)
             (let ((access-token (cl-oauth:obtain-access-token *get-access-token-endpoint* request-token)))
-              (with-open-file (f "~/.g-tumblr-config.lisp" :direction :output :if-exists :new-version)
-                (write (list :consumer-key (cl-oauth:token-key consumer-token)
+              (with-open-file (f *config-pathname* :direction :output :if-exists :new-version)
+                (write (list :blog-name blog-name
+                             :consumer-key (cl-oauth:token-key consumer-token)
                              :consumer-secret (cl-oauth:token-secret consumer-token)
                              :access-key (cl-oauth:token-key access-token)
                              :access-secret (cl-oauth:token-secret access-token))
                        :stream f)))))))))
 
+(defun read-configuration ()
+  (with-open-file (f *config-pathname*)
+    (destructuring-bind (&key blog-name consumer-key consumer-secret access-key access-secret) (read f)
+      (list :blog-name blog-name
+            :access-token (make-instance 'cl-oauth:access-token
+                                         :consumer (make-instance 'cl-oauth:consumer-token
+                                                                  :key consumer-key
+                                                                  :secret consumer-secret)
+                                         :key access-key
+                                         :secret access-secret)))))
+
+(defun url-encode-octets (octets)
+  "URL-encode a string."
+  (with-output-to-string (s)
+    (loop for octet across octets
+          for c = (code-char octet)     ; ugh
+          do (cond
+               ((or (char<= #\0 c #\9)
+                    (char<= #\a c #\z)
+                    (char<= #\A c #\Z)
+                    (find c "$-_.!*'()," :test #'char=))
+                (write-char c s))
+               (t (format s "%~2,'0x" octet))))))
+
 (defun post-image (pathname)
   (format t "pathname: ~A~%" pathname)
   (temporary-file:with-open-temporary-file (rescaled-image-pathname :template (format nil "temporary-files:%.~A" (pathname-type pathname)))
-    (normalize-image pathname rescaled-image-pathname)))
+    (normalize-image pathname rescaled-image-pathname)
+    (let ((drakma:*text-content-types* '(("text" . nil) ("application" . "json"))))
+      (destructuring-bind (&key blog-name access-token) (read-configuration)
+        (cl-oauth:access-protected-resource (format nil "https://api.tumblr.com/v2/blog/~A.tumblr.com/post" blog-name)
+                                            access-token
+                                            :request-method :post
+                                            :user-parameters `(("type" . "text")
+                                                               ("body" . "he")
+                                                               ;; status: with data[0] parameters, 401
+                                                               ("data[0]" . ,(url-encode-octets (alexandria:read-file-into-byte-vector rescaled-image-pathname)))))))))
 
 (defun process-print-requests (pathname)
   (let ((file (dpof:parse-file pathname)))
@@ -119,7 +154,7 @@
                                    (make-pathname :name "AUTPRINT"
                                                   :type "MRK"
                                                   :directory `(:relative ,name "MISC"))
-                                  #P"/Volumes/")))
+                                   #P"/Volumes/")))
     (process-print-requests pathname)))
 
 (defun main ()
